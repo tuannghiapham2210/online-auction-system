b5255a4595cd2c2e2152171e8f3a334c9518b72d
Phạm Tuấn Nghĩa <tuannghiapham2210@gmail.com>
2026-04-11 09:05:24 +0700

    tạo kiến trúc dự án + giao diện client

---
+++ online-auction-client/src/main/java/com/auction/App.java
@@ -0,0 +1,32 @@
+package com.auction;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        // Lệnh này đi tìm file fxml và phân tích cú pháp cây (Tree) bên trong nó
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("login.fxml"));
+        Parent root = fxmlLoader.load();
+        
+        // Bọc cái Root Node đó vào một Scene (bối cảnh) kích thước 640x480
+        scene = new Scene(root, 640, 480);
+        
+        stage.setTitle("Hệ Thống Đấu Giá Trực Tuyến");
+        stage.setScene(scene);
+        stage.show(); // Hiển thị cửa sổ
+    }
+
+    public static void main(String[] args) {
+        launch(); 
+    }
+}
