38629265e2ca2a9b81cc6bdb0af5134c4f75efd9
Phạm Tuấn Nghĩa <tuannghiapham2210@gmail.com>
2026-05-05 14:30:25 +0700

    thêm folder test cho bên server + thêm dependency cho Junit

---
+++ online-auction-server/src/test/java/com/auction/factory/ItemFactoryTest.java
@@ -0,0 +1,9 @@
+package com.auction.factory;
+
+import com.auction.model.Art;
+import com.auction.model.Electronics;
+import com.auction.model.Item;
+import com.auction.model.Vehicle;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
