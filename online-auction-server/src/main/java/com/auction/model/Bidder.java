546f2db81562926bf25f47ed13f6d5aecbc02f7e
TrinhHung1510-me <trinhquang133@gmail.com>
2026-05-05 22:01:07 +0700

    Replace System.out.println with SLF4J logging and translate Vietnamese strings to English

--- online-auction-server/src/main/java/com/auction/model/Bidder.java
+++ online-auction-server/src/main/java/com/auction/model/Bidder.java
@@ -1,12 +1,17 @@
 package com.auction.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Bidder extends User {
+    private static final Logger logger = LoggerFactory.getLogger(Bidder.class);
+
     public Bidder(String username, String password) {
         super(username, password, "BIDDER");
     }
 
     @Override
     public void displayRoleMenu() {
-        System.out.println("Giao diện: Xem danh sách đấu giá, Đặt giá");
+        logger.info("Interface: View auction list, Place bids");
     }
 }
