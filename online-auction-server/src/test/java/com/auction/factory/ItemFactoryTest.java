3e66c677f86144735c4e1dfd9f4813ced97d61de
TunaEm <trandanhtuananh@gmail.com>
2026-05-06 15:18:14 +0700

    them electronic test

--- online-auction-server/src/test/java/com/auction/factory/ItemFactoryTest.java
+++ online-auction-server/src/test/java/com/auction/factory/ItemFactoryTest.java
@@ -20,4 +20,20 @@ class ItemFactoryTest {
         Item item = ItemFactory.createItem("", "Phone", 100.0, "2026-12-31", 1, "12");
         assertNull(item);
     }
+    @Test
+    void createItem_electronics_returnsElectronics() {
+        Item item = ItemFactory.createItem("ELECTRONICS", "Phone", 100.0, "2026-12-31", 10, "18");
+
+        assertNotNull(item);
+        assertInstanceOf(Electronics.class, item);
+        assertEquals("ELECTRONICS", item.getItemType());
+        assertEquals("18", item.getExtraInfo());
+
+        Electronics electronics = (Electronics) item;
+        assertEquals("Phone", electronics.getName());
+        assertEquals(100.0, electronics.getStartingPrice());
+        assertEquals("2026-12-31", electronics.getEndTime());
+        assertEquals(10, electronics.getSellerId());
+        assertEquals(18, electronics.getWarranty());
+    }
 }
