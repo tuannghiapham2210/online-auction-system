ea1e68bb41f34f5087d218a2c6dde06e3527a81b
tknamvn <trankhanhnamvn@gmail.com>
2026-05-27 00:33:23 +0700

    Refactor Models, reorganize files, create common folder

--- online-auction-client/src/main/java/com/auction/model/Entity.java
+++
@@ -1,8 +0,0 @@
-package com.auction.model;
-
-public abstract class Entity {
-    protected int id; // Dùng protected để các lớp con kế thừa được
-
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-}
